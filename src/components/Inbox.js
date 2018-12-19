1d7488b9d0bce362d2155779c1f1b9daa5ca4f06
wuxingheng <wuxh@wuxinghengdeMacBook-Pro.local>
2018-12-19 22:34:25 +0800

    优化开发环境，加速开发编译

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -1,114 +1,111 @@
-import React from 'react'
-import axios from 'axios';
-import json3 from 'json3'
-import '../media/css/test.less'
-import title from '../media/images/title.png'
-import DatePicker from 'antd/lib/date-picker'
+import React from "react";
+import axios from "axios";
+import json3 from "json3";
+import "../media/css/test.less";
+import title from "../media/images/title.png";
+import DatePicker from "antd/lib/date-picker";
 
 class Inbox extends React.Component {
-    state = {
-        data: null
-    }
+  state = {
+    data: null
+  };
 
-    componentDidMount() {
-        console.log('发送请求')
-        axios.get('http://jsonplaceholder.typicode.com/posts?userId=1').then((result) => {
-            console.log(result)
-            // console.log(json3.parse(result.data))
-            console.log('set State')
-            if (result.data.code === 1) {
-                this.setState({
-                    data: result.data
-                })
-            } else {
-                this.setState({
-                    data: result.data
-                })
-            }
-        }).catch((err) => {
-            console.log(err)
-        })
-        let obj =  {name: [1, 2], age: 2}
-        Object.keys(obj).forEach((item) => {
-            if (item === 'name') {
-                if (obj[item].includes(2)) {
-                    console.log(1)
-                } else {
-                    console.log(3)
-                }
-            } else {
-                console.log(obj[item])
-            }
-        })
-    }
+  componentDidMount() {
+    console.log("发送请求");
+    axios
+      .get("http://jsonplaceholder.typicode.com/posts?userId=1")
+      .then(result => {
+        console.log(result);
+        // console.log(json3.parse(result.data))
+        console.log("set State");
+        if (result.data.code === 1) {
+          this.setState({
+            data: result.data
+          });
+        } else {
+          this.setState({
+            data: result.data
+          });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    let obj = { name: [1, 2], age: 2 };
+    Object.keys(obj).forEach(item => {
+      if (item === "name") {
+        if (obj[item].includes(2)) {
+          console.log(1);
+        } else {
+          console.log(3);
+        }
+      } else {
+        console.log(obj[item]);
+      }
+    });
+  }
 
-    handleClick = () => {
-        let {history} = this.props
-        history.push('/inbox/messages/1')
-    }
+  handleClick = () => {
+    let { history } = this.props;
+    history.push("/inbox/messages/1");
+  };
 
-    handleCount = () => {
-        let {history} = this.props
-        history.push('/count')
-    }
+  handleCount = () => {
+    let { history } = this.props;
+    history.push("/count");
+  };
 
-    handleVideo = () => {
-        let {history} = this.props
-        history.push('/video')
-    }
+  handleVideo = () => {
+    let { history } = this.props;
+    history.push("/video");
+  };
 
-    handleVideo2 = () => {
-        let {history} = this.props
-        history.push('/video2')
-    }
+  handleVideo2 = () => {
+    let { history } = this.props;
+    history.push("/video2");
+  };
 
-    handleVideo3 = () => {
-        let {history} = this.props
-        history.push('/video3')
-    }
+  handleVideo3 = () => {
+    let { history } = this.props;
+    history.push("/video3");
+  };
 
-    handleVideo4 = () => {
-        let {history} = this.props
-        history.push('/video4')
-    }
+  handleVideo4 = () => {
+    let { history } = this.props;
+    history.push("/video4");
+  };
 
-    handleChange = (value, dateString) => {
-        console.log(value, dateString)
-    }
+  handleChange = (value, dateString) => {
+    console.log(value, dateString);
+  };
 
-    render() {
-        console.log(this.props)
-        let {data} = this.state
-        return (
-            <div>
-                <h2>Inbox</h2>
-                <img src={title} />
-                <button onClick={this.handleClick}>
-                    <span>go messages</span>
-                </button>
-                <button onClick={this.handleCount}>go count</button>
-                <button onClick={this.handleVideo}>go video</button>
-                <button onClick={this.handleVideo2}>go video2</button>
-                <button onClick={this.handleVideo3}>go video3</button>
-                <button onClick={this.handleVideo4}>go video4</button>
-                {this.props.children || "Welcome to your Inbox"}
-                {
-                    data? (
-                        data.map((item) => {
-                            return (
-                                <div key={item.id}>
-                                    {item.title + '-' + item.id}
-                                </div>
-                            )
-                        })
-                    ) : (
-                        <div>没有setState</div>
-                    )
-                }
-                <DatePicker onChange={this.handleChange} />
-            </div>
-        )
-    }
+  render() {
+    console.log(this.props);
+    let { data } = this.state;
+    return (
+      <div>
+        <h2>Inbox</h2>
+        <img src={title} />
+        <button onClick={this.handleClick}>
+          <span>go messages</span>
+        </button>
+        <button onClick={this.handleCount}>go count</button>
+        <button onClick={this.handleVideo}>go video</button>
+        <button onClick={this.handleVideo2}>go video2</button>
+        <button onClick={this.handleVideo3}>go video3</button>
+        <button onClick={this.handleVideo4}>go video4</button>
+        {this.props.children || "Welcome to your Inbox"}
+        {data ? (
+          data.map(item => {
+            return <div key={item.id}>{item.title + "-" + item.id}</div>;
+          })
+        ) : (
+          <div>没有setState</div>
+        )}
+        <DatePicker onChange={this.handleChange} />
+      </div>
+    );
+  }
 }
 
-export default Inbox
+export default Inbox;
--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,45 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const es3ifyPlugin = require('es3ify-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const es3ifyPlugin = require("es3ify-webpack-plugin");
 
-const args = process.argv.slice(2)
-const DEBUG = !(args[0] === '--release')
-const VERBOSE = args[0] === '--verbose'
+const args = process.argv.slice(2);
+const DEBUG = !(args[0] === "--release");
+const VERBOSE = args[0] === "--verbose";
 
-/**
- * Webpack configuration (core/main.js => build/bundle.js)
- * http://webpack.github.io/docs/configuration.html
- */
 const config = {
+  context: path.resolve(__dirname, "./src"),
 
-  // The base directory
-  context: path.resolve(__dirname, './src'),
-
-  // The entry point for the bundle
   entry: {
-    app: [
-      './main.js',
-    ],
-    vendor: [ 
-      'es5-shim',
-      'es5-shim/es5-sham',
-      'babel-polyfill',
-      'es6-promise',
-      'fetch-detector',
-      'fetch-ie8',
-      'fetch-jsonp',
-      'react',
-      'react-dom',
-      'react-redux',
-      'react-router',
-    ],
+    app: ["./main.js"],
+    vendor: [
+      "es5-shim",
+      "es5-shim/es5-sham",
+      "babel-polyfill",
+      "es6-promise",
+      "react",
+      "react-dom",
+      "react-redux",
+      "react-router"
+    ]
   },
 
-  // Options affecting the output of the compilation
   output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/',
-    filename: 'assets/[name].js',
-    chunkFilename: 'assets/[name].js',
-    sourcePrefix: '  ',
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "assets/[name].js",
+    chunkFilename: "assets/[name].js",
+    sourcePrefix: "  "
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
   },
 
-  // Switch loaders to debug or release mode
   debug: DEBUG,
   cache: DEBUG,
 
-  // Developer tool to enhance debugging, source maps
-  // http://webpack.github.io/docs/configuration.html#devtool
-  devtool: DEBUG ? 'source-map' : false,
-
-  // What information should be printed to the console
+  devtool: DEBUG ? "source-map" : false,
   stats: {
     colors: true,
     reasons: DEBUG,
@@ -65,146 +50,140 @@ const config = {
     chunkModules: VERBOSE,
     cached: VERBOSE,
     cachedAssets: VERBOSE,
-    children: false,
+    children: false
   },
-
-  // The list of plugins for Webpack compiler
   plugins: [
-    new es3ifyPlugin(),
+    // new es3ifyPlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: Infinity,
+      name: "vendor",
+      minChunks: Infinity
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': DEBUG ? '"development"' : '"production"',
+      "process.env.NODE_ENV": DEBUG ? '"development"' : '"production"',
       __DEV__: DEBUG,
-      __BASENAME__: JSON.stringify(process.env.BASENAME || ''),
+      __BASENAME__: JSON.stringify(process.env.BASENAME || "")
     }),
-    new ExtractTextPlugin(
-      'assets/styles.css',
-      {
-        minimize: !DEBUG,
-        allChunks: true,
-      }
-    ),
+    new ExtractTextPlugin("assets/styles.css", {
+      minimize: !DEBUG,
+      allChunks: true
+    }),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.ejs'),
-      filename: 'index.html',
-      minify: !DEBUG ? {
-        collapseWhitespace: true,
-      } : null,
-      hash: true,
-    }),
+      template: path.resolve(__dirname, "./src/index.ejs"),
+      filename: "index.html",
+      minify: !DEBUG
+        ? {
+            collapseWhitespace: true
+          }
+        : null,
+      hash: true
+    })
   ],
-
-  // Options affecting the normal modules
   module: {
     loaders: [
       {
-        test: /\.jsx?$/,
-        include: [
-          path.resolve(__dirname, './src'),
-        ],
-        loader: 'babel-loader',
+        test: /\.(js|jsx)$/,
+        include: [path.resolve(__dirname, "./src")],
+        loader: "babel-loader",
         query: {
-          plugins: [],
-        },
+          plugins: []
+        }
       },
       {
         test: /\.css/,
         loader: ExtractTextPlugin.extract(
-          'style-loader',
-          'css-loader?-autoprefixer&modules=true&localIdentName=[local]!postcss-loader'
-        ),
+          "style-loader",
+          "css-loader?-autoprefixer&modules=true&localIdentName=[local]!postcss-loader"
+        )
       },
       {
         test: /\.less$/,
         loader: ExtractTextPlugin.extract(
-          'style-loader',
-          'css-loader?-autoprefixer!postcss-loader!less-loader'
-        ),
+          "style-loader",
+          "css-loader?-autoprefixer!postcss-loader!less-loader"
+        )
       },
       {
         test: /\.json$/,
-        loader: 'json-loader',
+        loader: "json-loader"
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         query: {
-          name: 'assets/[path][name].[ext]',
-          limit: 10000,
-        },
+          name: "assets/[path][name].[ext]",
+          limit: 10000
+        }
       },
       {
         test: /\.(eot|ttf|wav|mp3|ogg)$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         query: {
-          name: 'assets/[path][name].[ext]',
-        },
-      },
-    ],
+          name: "assets/[path][name].[ext]"
+        }
+      }
+    ]
   },
-
-
-
-  // Alias
   resolve: {
     alias: {
-      components: path.resolve(__dirname, './src/components/'),
-      routes: path.resolve(__dirname, './src/routes/'),
-      services: path.resolve(__dirname, './src/services/'),
-      store: path.resolve(__dirname, './src/store/'),
-    },
-  },
-}
-
-// Optimize the bundle in release (production) mode
-if (!DEBUG) {
-  config.plugins.push(new webpack.optimize.DedupePlugin())
-}
-
-// https://github.com/jun0205/react-static-boilerplate/issues/14
-// Must always Uglify somehow or it won't work in IE8
-const uglyOptions = !DEBUG ? {
-  compress: {
-    warnings: VERBOSE,
-    screw_ie8: false,
-  },
-  mangle: { screw_ie8: false },
-  output: { screw_ie8: false },
-} : {
-    mangle: false,
-    compress: {
-      drop_debugger: false,
-      warnings: VERBOSE,
-      screw_ie8: false,
-    },
-    output: {
-      beautify: true,
-      comments: true,
-      bracketize: true,
-      indent_level: 2,
-      keep_quoted_props: true,
-      screw_ie8: false,
-    },
+      components: path.resolve(__dirname, "./src/components/"),
+      routes: path.resolve(__dirname, "./src/routes/"),
+      services: path.resolve(__dirname, "./src/services/"),
+      store: path.resolve(__dirname, "./src/store/")
+    }
   }
-
-config.plugins.push(new webpack.optimize.UglifyJsPlugin(uglyOptions))
+};
 
 if (!DEBUG) {
-  config.plugins.push(new webpack.optimize.AggressiveMergingPlugin())
-  config.module.loaders
-    .find(x => x.loader === 'babel-loader').query.plugins
-    .unshift(
-    'transform-react-remove-prop-types',
-    'transform-react-constant-elements',
-    'transform-react-inline-elements',
-    'transform-es3-modules-literals',
-    'transform-es3-member-expression-literals',
-    'transform-es3-property-literals'
-    )
+  config.plugins.push(new es3ifyPlugin());
+  config.plugins.push(new webpack.optimize.DedupePlugin());
 }
 
-module.exports = config
+const uglyOptions = !DEBUG
+  ? {
+      compress: {
+        warnings: VERBOSE,
+        screw_ie8: false
+      },
+      mangle: {
+        screw_ie8: false
+      },
+      output: {
+        screw_ie8: false
+      }
+    }
+  : {
+      mangle: false,
+      compress: {
+        drop_debugger: false,
+        warnings: VERBOSE,
+        screw_ie8: false
+      },
+      output: {
+        beautify: true,
+        comments: true,
+        bracketize: true,
+        indent_level: 2,
+        keep_quoted_props: true,
+        screw_ie8: false
+      }
+    };
+
+// config.plugins.push(new webpack.optimize.UglifyJsPlugin(uglyOptions))
+
+if (!DEBUG) {
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin(uglyOptions));
+  config.plugins.push(new webpack.optimize.AggressiveMergingPlugin());
+  config.module.loaders
+    .find(x => x.loader === "babel-loader")
+    .query.plugins.unshift(
+      "transform-react-remove-prop-types",
+      "transform-react-constant-elements",
+      "transform-react-inline-elements",
+      "transform-es3-modules-literals",
+      "transform-es3-member-expression-literals",
+      "transform-es3-property-literals"
+    );
+}
+
+module.exports = config;
